adb659f76b78d471c5a78462ba14868aaa404a44
Sergio Adame <sergioadameva@gmail.com>
2022-04-04 17:30:18 -0500

    Updates

---
+++ precache-manifest.7d626282963e35ef7a7c025597aa9a3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251b5bd778a8b4acbd31b735d7dd11b7",
+    "url": "/test2/index.html"
+  },
+  {
+    "revision": "a4edc1005ffefe8de35c",
+    "url": "/test2/static/css/main.8ae46512.chunk.css"
+  },
+  {
+    "revision": "25fa5106a9a928f8e3e4",
+    "url": "/test2/static/js/2.8f4d6a19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test2/static/js/2.8f4d6a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4edc1005ffefe8de35c",
+    "url": "/test2/static/js/main.1865c9c0.chunk.js"
+  },
+  {
+    "revision": "998a195b7e294ba19922",
+    "url": "/test2/static/js/runtime-main.661f25ff.js"
+  },
+  {
+    "revision": "3a60e3be16c3d7ff5e23b7501cf13afc",
+    "url": "/test2/static/media/AnconaAutopartes.3a60e3be.png"
+  }
+]);
